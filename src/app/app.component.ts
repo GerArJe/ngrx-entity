7a53cf06f79a9f7e4f9ab621e130db108d77f28a
German Arevalo J <germanarevalojerez@gmail.com>
2023-12-29 15:37:37 -0500

    feat: :sparkles: implement entity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,129 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from './state/user/user.model';
+import { State } from './state/user/user.reducer';
+import { Store } from '@ngrx/store';
+import * as fromUser from './state/user/user.reducer';
+import { randFullName, randUuid } from '@ngneat/falso';
+import { UserActions } from './state/user/user.actions';
+import { Update } from '@ngrx/entity';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngrx-entity';
+  users$: Observable<User[]>;
+  userId: string = '';
+  userId2: string = '';
+
+  constructor(private store: Store<State>) {
+    this.users$ = this.store.select(fromUser.selectAll);
+  }
+
+  addOne() {
+    const user: User = {
+      id: randUuid(),
+      name: randFullName(),
+    };
+    this.store.dispatch(UserActions.addUser({ user }));
+    this.userId = user.id;
+  }
+
+  upsertUser() {
+    const user: User = {
+      id: randUuid(),
+      name: randFullName(),
+    };
+    this.store.dispatch(UserActions.upsertUser({ user }));
+  }
+
+  addUsers() {
+    const users: User[] = [
+      {
+        id: randUuid(),
+        name: randFullName(),
+      },
+      {
+        id: randUuid(),
+        name: randFullName(),
+      },
+    ];
+    this.store.dispatch(UserActions.addUsers({ users }));
+    this.userId = users[0].id;
+    this.userId2 = users[1].id;
+  }
+
+  upsertUsers() {
+    const users: User[] = [
+      {
+        id: randUuid(),
+        name: randFullName(),
+      },
+      {
+        id: randUuid(),
+        name: randFullName(),
+      },
+    ];
+    this.store.dispatch(UserActions.upsertUsers({ users }));
+  }
+
+  updateUser() {
+    const user: Partial<User> = {
+      name: `${randFullName()} (updated)`,
+    };
+    this.store.dispatch(
+      UserActions.updateUser({ user: { id: this.userId, changes: user } })
+    );
+  }
+
+  updateUsers() {
+    const users: Update<User>[] = [
+      {
+        id: this.userId,
+        changes: {
+          name: `${randFullName()} (updated 1)`,
+        },
+      },
+      {
+        id: this.userId2,
+        changes: {
+          name: `${randFullName()} (updated 2)`,
+        },
+      },
+    ];
+    this.store.dispatch(UserActions.updateUsers({ users }));
+  }
+
+  deleteUser() {
+    this.store.dispatch(UserActions.deleteUser({ id: this.userId }));
+  }
+
+  deleteUsers() {
+    this.store.dispatch(UserActions.deleteUsers({ ids: [this.userId, this.userId2] }));
+  }
+
+  loadUsers() {
+    const users: User[] = [
+      {
+        id: randUuid(),
+        name: randFullName(),
+      },
+      {
+        id: randUuid(),
+        name: randFullName(),
+      },
+    ];
+    this.store.dispatch(UserActions.loadUsers({ users }));
+  }
+
+  clearUsers() {
+    this.store.dispatch(UserActions.clearUsers());
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import * as fromUser from './state/user/user.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode(), name: 'ngrx-entity' })]
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideState(fromUser.usersFeatureKey, fromUser.reducer),
+    provideEffects(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      name: 'ngrx-entity',
+    }),
+  ],
 };
